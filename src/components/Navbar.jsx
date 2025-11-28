693d6d57b9c24e4cc0f56046e92f2c125f05eddb
Felipe <felipesantacruz.ads@gmail.com>
2025-11-28 20:59:18 -0300

    adding simple logo and adjusting navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import iassistenteLogo from "../../assets/iassistente.png";
 
 export default function Navbar() {
   return (
     <header className="w-full bg-white shadow-sm border-b border-gray-200">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link to="/" className="text-xl font-semibold tracking-tight text-gray-800 hover:text-blue-600">
-          MarketDashboard
+          <img src={iassistenteLogo} width='200 rem' alt="logo iassistente" />
         </Link>
 
-        <div className="flex items-center gap-6 text-gray-600">
+        <div className="flex items-center text-2xl gap-6 text-gray-600">
           <Link to="/crypto" className="hover:text-blue-600">Cripto</Link>
           <Link to="/stocks" className="hover:text-blue-600">Ações</Link>
         </div>
